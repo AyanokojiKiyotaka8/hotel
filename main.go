64b29085d8bc08355f86eeadab3a684d29380c08
AyanokojiKiyotaka8 <ayanokojikiyotakakingbest@gmail.com>
2024-12-01 15:17:23 +0500

    Creating user and small handlers

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+
+	"github.com/AyanokojiKiyotaka8/booking.git/api"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println("hello")
+	listenAddr := flag.String("listenAddr", ":3000", "The listen address of the API server")
+	flag.Parse()
+
+	app := fiber.New()
+	apiv1 := app.Group("/api/v1")
+
+	apiv1.Get("/user", api.HandleGetUsers)
+	apiv1.Get("/user/:id", api.HandleGetUser)
+
+	app.Listen(*listenAddr)
 }
